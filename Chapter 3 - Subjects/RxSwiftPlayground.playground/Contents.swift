fff2d7a794537370d4e538b21ce5871f1d6e841c
Kostiantyn Dovbush <kostiantyn.dovbush@softnauts.com>
2019-09-23 13:46:21 +0200

    Chapter 3 - Code from the book

--- Chapter 3 - Subjects/RxSwiftPlayground.playground/Contents.swift
+++ Chapter 3 - Subjects/RxSwiftPlayground.playground/Contents.swift
@@ -1,4 +1,109 @@
 //: Please build the scheme 'RxSwiftPlayground' first
 import RxSwift
 
-print("test")
+//example(of: "PublishSubject") {
+//    let subject = PublishSubject<String>()
+//    subject.onNext("Is anyone listening?")
+//
+//    let subscriptionOne = subject
+//        .subscribe(onNext: { string in
+//            print("1) \(string)")
+//    })
+//
+//    subject.on(.next("1"))
+//    subject.onNext("2")
+//
+//    let subscriptionTwo = subject.subscribe { event in
+//        print("2)", event.element ?? event)
+//    }
+//
+//    subject.onNext("3")
+//    subscriptionOne.dispose()
+//    subject.onNext("4")
+//
+//    subject.onCompleted()
+//
+//    subject.onNext("5")
+//
+//    subscriptionTwo.dispose()
+//
+//    let disposeBag = DisposeBag()
+//
+//    subject.subscribe {
+//        print("3)", $0.element ?? $0)
+//    }.disposed(by: disposeBag)
+//
+//    subject.onNext("?")
+//}
+
+enum MyError: Error {
+    case anError
+}
+
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    print(label, event.element ?? event.error ?? event)
+}
+//
+//example(of: "BehaviourSubject") {
+//    let subject = BehaviorSubject(value: "Initial value")
+//    let disposeBag = DisposeBag()
+//
+//    subject.onNext("X")
+//
+//    subject.subscribe {
+//        print(label: "1)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    subject.onError(MyError.anError)
+//
+//    subject.subscribe {
+//        print(label: "2)", event: $0)
+//    }.disposed(by: disposeBag)
+//}
+//
+//example(of: "ReplaySubject") {
+//    let subject = ReplaySubject<String>.create(bufferSize: 2)
+//    let disposeBag = DisposeBag()
+//
+//    subject.onNext("1")
+//    subject.onNext("2")
+//    subject.onNext("3")
+//
+//    subject.subscribe {
+//        print(label: "1)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    subject.subscribe {
+//        print(label: "2)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    subject.onNext("4")
+//
+//    subject.onError(MyError.anError)
+//    subject.dispose()
+//
+//    subject.subscribe {
+//        print(label: "3)", event: $0)
+//    }.disposed(by: disposeBag)
+//}
+
+//example(of: "Variable") {
+//    let variable = Variable("Initial value")
+//    let disposeBag = DisposeBag()
+//
+//    variable.value = "New initial value"
+//
+//    variable.asObservable().subscribe {
+//        print(label: "1)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    variable.value = "1"
+//
+//    variable.asObservable().subscribe {
+//        print(label: "2)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    variable.value = "2"
+//
+//
+//}
