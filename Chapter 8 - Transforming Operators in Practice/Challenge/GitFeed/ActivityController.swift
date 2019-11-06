70edfb5e896c0706b4b2933de5c62a44c7124181
Kostiantyn Dovbush <kostiantyn.dovbush@gmail.com>
2019-11-06 08:14:53 +0100

    Chapter 8 - Code

---
+++ Chapter 8 - Transforming Operators in Practice/Challenge/GitFeed/ActivityController.swift
@@ -0,0 +1,153 @@
+/*
+* Copyright (c) 2016-present Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+* distribute, sublicense, create a derivative work, and/or sell copies of the
+* Software in any work that is designed, intended, or marketed for pedagogical or
+* instructional purposes related to programming, coding, application development,
+* or information technology.  Permission for such use, copying, modification,
+* merger, publication, distribution, sublicensing, creation of derivative works,
+* or sale is expressly withheld.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
+
+class ActivityController: UITableViewController {
+	private let repo = "ReactiveX/RxSwift"
+	
+	private let events = BehaviorRelay<[Event]>(value: [])
+	private let bag = DisposeBag()
+	private lazy var eventsFileURL = {
+		return FileManager.default
+			.urls(for: .cachesDirectory, in: .allDomainsMask)
+			.first!
+			.appendingPathComponent("events.json")
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = repo
+		
+		self.refreshControl = UIRefreshControl()
+		let refreshControl = self.refreshControl!
+		
+		refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+		refreshControl.tintColor = UIColor.darkGray
+		refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+		refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+		
+		let decoder = JSONDecoder()
+		if let eventsData = try? Data(contentsOf: eventsFileURL), let persistedEvents = try? decoder.decode([Event].self, from: eventsData) {
+			events.accept(persistedEvents)
+			self.tableView.reloadData()
+		}
+		
+		refresh()
+	}
+	
+	@objc func refresh() {
+		DispatchQueue.global(qos: .default).async { [weak self] in
+			guard let self = self else { return }
+			self.fetchEvents(repo: self.repo)
+		}
+	}
+	
+	func fetchEvents(repo: String) {
+		let response = Observable.from(["https://api.github.com/search/repositories?q=language:swift&per_page=5"])
+		.map { urlString -> URL in
+			return URL(string: urlString)!
+		}
+		.flatMap { url -> Observable<Any> in
+			let request = URLRequest(url: url)
+			return URLSession.shared.rx.json(request: request)
+		}
+		.flatMap { response -> Observable<String> in
+			guard let response = response as? [String: Any],
+						let items = response["items"] as? [[String: Any]] else {
+				return Observable.empty()
+			}
+
+			return Observable.from(items.map { $0["full_name"] as! String })
+		}
+		.map { urlString -> URL in
+			return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+		}
+		.map {
+			URLRequest(url: $0)
+		}
+		.flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+			return URLSession.shared.rx.response(request: request)
+		}
+		.share(replay: 1)
+		
+		response
+			.filter { response, _ in
+				return 200..<300 ~= response.statusCode
+			}
+			.map { _, data -> [Event] in
+				let decoder = JSONDecoder()
+				let events = try? decoder.decode([Event].self, from: data)
+				return events ?? []
+			}
+			.filter { objects in
+				return !objects.isEmpty
+			}
+			.subscribe(onNext: { [weak self] newEvents in
+				self?.processEvents(newEvents)
+			})
+			.disposed(by: bag)
+	}
+	
+	func processEvents(_ newEvents: [Event]) {
+		var updatedEvents = newEvents + events.value
+		if updatedEvents.count > 50 {
+			updatedEvents = [Event](updatedEvents.prefix(upTo: 50))
+		}
+		events.accept(updatedEvents)
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+			self.refreshControl?.endRefreshing()
+		}
+		
+		let encoder = JSONEncoder()
+		if let eventsData = try? encoder.encode(updatedEvents) {
+			try? eventsData.write(to: eventsFileURL, options: .atomicWrite)
+		}
+	}
+	
+	// MARK: - Table Data Source
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return events.value.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let event = events.value[indexPath.row]
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+		cell.textLabel?.text = event.actor.name
+		cell.detailTextLabel?.text = event.repo.name + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+		cell.imageView?.kf.setImage(with: event.actor.avatar, placeholder: UIImage(named: "blank-avatar"))
+		return cell
+	}
+}
